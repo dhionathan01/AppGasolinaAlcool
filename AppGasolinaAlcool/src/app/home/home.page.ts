2217e1445adc40f044f29ccaf503023f140a6b21
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-12-30 00:36:39 -0300

    feat: :sparkles: Validando se gasolina é melhor que alcool

--- AppGasolinaAlcool/src/app/home/home.page.ts
+++ AppGasolinaAlcool/src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public precoAlcool: string = "";
+  public precoGasolina: string = "";
+  public resultado: string = "Resultado";
+  constructor() { }
+  public calcular() {
+    // Validar se os campos foram preenchidos
+    if (this.precoAlcool && this.precoGasolina) {
+      let pAlcool = parseFloat(this.precoAlcool);
+      let pGasolina = parseFloat(this.precoGasolina);
+
+      let resultado = pAlcool / pGasolina;
+
+      if (resultado >= 0.7) {
+        this.resultado = "Melhor utilizar Gasolina";
+      } else {
+        this.resultado = "Melhor utilizar Álcool";
+      }
+
+    } else {
+      this.resultado = "Preencha corretamente os campos!";
+    }
+  }
 
 }
